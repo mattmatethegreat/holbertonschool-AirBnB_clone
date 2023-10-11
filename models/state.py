e8ef495f2b89d8cf7197613db1a9adc36e80014d
BrokenUzi <jcup143@gmail.com>
2023-10-10 19:04:48 -0700

    Added simple code to state

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """ State Class """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = ""
