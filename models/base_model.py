a1c1b64ecf6ea9bf7d4b42d2abb99045bb572ce0
BrokenUzi <jcup143@gmail.com>
2023-10-10 17:11:39 -0700

    Added rough draft of basemodels

---
+++ models/base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+""" Base Model Class """
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def to_dict(self):
+        model_dict = dict(self.__dict__)
+        model_dict["__class__"] = self.__class__.__name__
+        model_dict["created_at"] = self.created_at.isoformat()
+        model_dict["updated_at"] = self.updated_at.isoformat()
+        return model_dict
